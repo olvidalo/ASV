6f0e85dcbea22f06421c983ccec763031a179dfc
Twan Goosen <t.goosen@gmail.com>
2013-04-02 07:49:00 +0000

    Created mock implementations for all services and models in API, and beans from this for use in the test packages

--- metadata-browser-pages/src/main/java/nl/mpi/metadatabrowser/wicket/HomePage.java
+++ metadata-browser-pages/src/main/java/nl/mpi/metadatabrowser/wicket/HomePage.java
@@ -1,12 +1,22 @@
 package nl.mpi.metadatabrowser.wicket;
 
+import nl.mpi.metadatabrowser.services.NodeActionsProvider;
+import nl.mpi.metadatabrowser.services.NodePresentationProvider;
+import nl.mpi.metadatabrowser.services.NodeTypeIdentifier;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
 public class HomePage extends WebPage {
 
     private static final long serialVersionUID = 1L;
+    @SpringBean
+    private NodeTypeIdentifier nodeTypeIdentifier;
+    @SpringBean
+    private NodeActionsProvider nodeActionsProvider;
+    @SpringBean
+    private NodePresentationProvider nodePresentationProvider;
 
     public HomePage(final PageParameters parameters) {
 	add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
