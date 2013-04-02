013ddd4b62fe6cc24610fbfbfcfc930b5038c60c
Twan Goosen <t.goosen@gmail.com>
2013-04-02 07:30:36 +0000

    Added wicket id generally required for construction of component to getNodePresentation method of presentation provider interface

--- metadata-browser-api/src/main/java/nl/mpi/metadatabrowser/services/NodePresentationProvider.java
+++ metadata-browser-api/src/main/java/nl/mpi/metadatabrowser/services/NodePresentationProvider.java
@@ -26,5 +26,5 @@ import org.apache.wicket.Component;
  */
 public interface NodePresentationProvider {
 
-    Component getNodePresentation(URI nodeUri, NodeType nodeType);
+    Component getNodePresentation(String wicketId, URI nodeUri, NodeType nodeType);
 }
