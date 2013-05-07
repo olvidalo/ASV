579c09db46932ac92ef969d8ab9bf336335039ff
Twan Goosen <t.goosen@gmail.com>
2013-05-07 11:12:21 +0000

    Added support for node icon provider and created a mock

--- metadata-browser-pages/src/main/java/nl/mpi/metadatabrowser/wicket/HomePage.java
+++ metadata-browser-pages/src/main/java/nl/mpi/metadatabrowser/wicket/HomePage.java
@@ -1,11 +1,12 @@
 package nl.mpi.metadatabrowser.wicket;
 
-import nl.mpi.metadatabrowser.wicket.components.NodesPanel;
 import java.io.Serializable;
 import nl.mpi.archiving.tree.CorpusNode;
 import nl.mpi.archiving.tree.GenericTreeModelProvider;
+import nl.mpi.archiving.tree.wicket.components.ArchiveTreeNodeIconProvider;
 import nl.mpi.archiving.tree.wicket.components.ArchiveTreePanel;
 import nl.mpi.archiving.tree.wicket.components.ArchiveTreePanelListener;
+import nl.mpi.metadatabrowser.wicket.components.NodesPanel;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.model.util.CollectionModel;
@@ -17,12 +18,14 @@ public class HomePage<SerializableCorpusNode extends CorpusNode & Serializable> 
     private static final long serialVersionUID = 1L;
     @SpringBean
     private GenericTreeModelProvider treeModelProvider;
+    @SpringBean(required = false)
+    private ArchiveTreeNodeIconProvider<CorpusNode> treeIconProvider;
     private NodesPanel nodesPanel;
 
     public HomePage(final PageParameters parameters) {
 	super();
 
-	final ArchiveTreePanel treePanel = new ArchiveTreePanel("treePanel", treeModelProvider);
+	final ArchiveTreePanel treePanel = new ArchiveTreePanel("treePanel", treeModelProvider, treeIconProvider);
 	add(treePanel);
 
 	nodesPanel = new NodesPanel("nodesPanel", new CollectionModel(treePanel.getSelectedNodes()));
---
+++ metadata-browser-pages/src/test/java/nl/mpi/archiving/tree/wicket/components/mock/MockNodeIconProvider.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2013 Max Planck Institute for Psycholinguistics
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.mpi.archiving.tree.wicket.components.mock;
+
+import nl.mpi.archiving.tree.CorpusNode;
+import nl.mpi.archiving.tree.wicket.components.ArchiveTreeNodeIconProvider;
+import nl.mpi.metadatabrowser.model.NodeType;
+import nl.mpi.metadatabrowser.services.NodeTypeIdentifier;
+import org.apache.wicket.request.resource.PackageResourceReference;
+import org.apache.wicket.request.resource.ResourceReference;
+
+/**
+ *
+ * @author Twan Goosen <twan.goosen@mpi.nl>
+ */
+public class MockNodeIconProvider<T extends CorpusNode> implements ArchiveTreeNodeIconProvider<T> {
+
+    private final ResourceReference sessionIcon = new PackageResourceReference(MockNodeIconProvider.class, "session_color.gif");
+    private final ResourceReference corpusIcon = new PackageResourceReference(MockNodeIconProvider.class, "corpusnode_color.gif");
+    private final ResourceReference fileIcon = new PackageResourceReference(MockNodeIconProvider.class, "mediafile.gif");
+    private final NodeTypeIdentifier nodeTypeIdentifier;
+
+    public MockNodeIconProvider(NodeTypeIdentifier nodeTypeIdentifier) {
+	this.nodeTypeIdentifier = nodeTypeIdentifier;
+    }
+
+    @Override
+    public ResourceReference getNodeIcon(T contentNode) {
+	final NodeType nodeType = nodeTypeIdentifier.getNodeType(contentNode);
+	if (nodeType.getName().equalsIgnoreCase("Collection")) {
+	    return corpusIcon;
+	} else if (nodeType.getName().equalsIgnoreCase("Root")) {
+	    return corpusIcon;
+	} else if (nodeType.getName().equalsIgnoreCase("Resource")) {
+	    return fileIcon;
+	} else {
+	    return sessionIcon;
+	}
+    }
+}
