77c947ba331574b1fcbab787e66351fae91feeda
Twan Goosen <t.goosen@gmail.com>
2013-10-23 08:16:16 +0000

    Cleaned up code in PanelViewNodeShowComponent

--- metadata-browser-cmdi/src/main/java/nl/mpi/metadatabrowser/model/cmdi/wicket/components/PanelViewNodeShowComponent.java
+++ metadata-browser-cmdi/src/main/java/nl/mpi/metadatabrowser/model/cmdi/wicket/components/PanelViewNodeShowComponent.java
@@ -1,24 +1,23 @@
 package nl.mpi.metadatabrowser.model.cmdi.wicket.components;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.net.URL;
 import javax.xml.transform.stream.StreamSource;
 import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.SaxonApiException;
 import net.sf.saxon.s9api.Serializer;
 import net.sf.saxon.s9api.XdmNode;
 import net.sf.saxon.s9api.XsltCompiler;
 import net.sf.saxon.s9api.XsltExecutable;
 import net.sf.saxon.s9api.XsltTransformer;
 import nl.mpi.archiving.corpusstructure.core.service.NodeResolver;
-import nl.mpi.metadatabrowser.model.NodeAction;
 import nl.mpi.metadatabrowser.model.TypedCorpusNode;
 import nl.mpi.metadatabrowser.services.NodePresentationException;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /*
  * Copyright (C) 2013 Max Planck Institute for Psycholinguistics
@@ -46,41 +45,41 @@ public final class PanelViewNodeShowComponent extends Panel {
     private final URL imdiXSLFile = getClass().getResource("/imdi-viewer.xsl");
 
     public PanelViewNodeShowComponent(String id, NodeResolver nodeResolver, TypedCorpusNode node) throws NodePresentationException {
-        super(id);
-        StringWriter strWriter = new StringWriter();
+	super(id);
 
-        final Processor proc = new Processor(false);
-        final XsltCompiler comp = proc.newXsltCompiler();
-        InputStream in;
-        
-        try {
-            in = nodeResolver.getInputStream(node);	// get the file
-            XsltExecutable exp;
-            if(nodeResolver.getUrl(node).toString().endsWith(".imdi")){
-                exp = comp.compile(new StreamSource(imdiXSLFile.getFile())); // compile the xslt imdi file
-            }else {
-             exp = comp.compile(new StreamSource(xslFile.getFile())); // compile the xslt file
-            }
-            XdmNode source = proc.newDocumentBuilder().build(
-                    new StreamSource(new InputStreamReader(in)));
-            final Serializer out = new Serializer();
-            out.setOutputProperty(Serializer.Property.METHOD, "html");
-            out.setOutputProperty(Serializer.Property.INDENT, "yes");
-            out.setOutputProperty(Serializer.Property.ENCODING, "UTF-8");
-            out.setOutputWriter(strWriter);
-            final XsltTransformer trans = exp.load();
+	final Processor proc = new Processor(false);
+	final XsltCompiler comp = proc.newXsltCompiler();
 
-            trans.setInitialContextNode(source);// parse xml via xslt
-            trans.setDestination(out);
-            trans.transform();
+	try {
+	    final InputStream in = nodeResolver.getInputStream(node);	// get the file
+	    final XsltExecutable exp;
+	    if (nodeResolver.getUrl(node).toString().endsWith(".imdi")) {
+		exp = comp.compile(new StreamSource(imdiXSLFile.getFile())); // compile the xslt imdi file
+	    } else {
+		exp = comp.compile(new StreamSource(xslFile.getFile())); // compile the xslt file
+	    }
+	    final XdmNode source = proc.newDocumentBuilder().build(
+		    new StreamSource(new InputStreamReader(in)));
+	    final Serializer out = new Serializer();
+	    final StringWriter strWriter = new StringWriter();
+	    out.setOutputWriter(strWriter);
+	    out.setOutputProperty(Serializer.Property.METHOD, "html");
+	    out.setOutputProperty(Serializer.Property.INDENT, "yes");
+	    out.setOutputProperty(Serializer.Property.ENCODING, "UTF-8");
+	    final XsltTransformer trans = exp.load();
 
-        } catch (Exception e) {
-            throw new NodePresentationException("Couldn't create IMDI or CMDI metadata: " + e.getMessage());
-        }
-        
-        // write to wicket the result of the parsing. Either return cmdi parsed or error message.
-        Label cmdiLabel = new Label("cmdiView", strWriter.toString());
-        cmdiLabel.setEscapeModelStrings(false);
-        add(cmdiLabel);
+	    trans.setInitialContextNode(source);// parse xml via xslt
+	    trans.setDestination(out);
+	    trans.transform();
+
+	    // write to wicket the result of the parsing - not escaping model string so as to pass through the verbatim HTML 
+	    final Label cmdiLabel = new Label("cmdiView", strWriter.toString());
+	    cmdiLabel.setEscapeModelStrings(false);
+	    add(cmdiLabel);
+	} catch (IOException ex) {
+	    throw new NodePresentationException("Could not read metadata for transformation", ex);
+	} catch (SaxonApiException ex) {
+	    throw new NodePresentationException("Could not transform metadata", ex);
+	}
     }
 }
