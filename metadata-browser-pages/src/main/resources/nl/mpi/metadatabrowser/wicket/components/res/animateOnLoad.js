45d0306585b7af1bbac59958c00b837a91882a02
Jean-Charles Ferrieres <Jean-Charles.Ferrieres@mpi.nl>
2013-11-20 14:57:42 +0000

    refs #3478 animation onload is stopped.

--- metadata-browser-pages/src/main/resources/nl/mpi/metadatabrowser/wicket/components/res/animateOnLoad.js
+++ metadata-browser-pages/src/main/resources/nl/mpi/metadatabrowser/wicket/components/res/animateOnLoad.js
@@ -15,11 +15,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
             $(function() {
-                var d = 300;
+             //   var d = 300;
                 $('#navigation a').each(function() {
                     $(this).stop().animate({
                         'marginTop': '-80px'
-                    }, d += 150);
+                    });
+                //    }, d += 150);
                 });
 
                 $('#navigation > li').hover(
