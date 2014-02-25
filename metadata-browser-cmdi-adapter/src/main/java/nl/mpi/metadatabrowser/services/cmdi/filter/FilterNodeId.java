2902d20452c832d18b10111e4f6e8d153554b37d
Jean-Charles Ferrieres <Jean-Charles.Ferrieres@mpi.nl>
2014-02-25 11:10:06 +0000

    refs #3719 implements FilterNodeIds interface bean added in corpustrcture.xml

---
+++ metadata-browser-cmdi-adapter/src/main/java/nl/mpi/metadatabrowser/services/cmdi/filter/FilterNodeId.java
@@ -0,0 +1,34 @@
+package nl.mpi.metadatabrowser.services.cmdi.filter;
+
+import java.net.URI;
+import nl.mpi.archiving.corpusstructure.adapter.AdapterUtils;
+import nl.mpi.metadatabrowser.services.FilterNodeIds;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/*
+ * Copyright (C) 2014 Max Planck Institute for Psycholinguistics
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ *
+ * @author Jean-Charles Ferrières <jean-charles.ferrieres@mpi.nl>
+ */
+public class FilterNodeId implements FilterNodeIds {
+
+    @Override
+    public String getURIParam(URI nodeid) {
+        return AdapterUtils.toNodeIdString(nodeid);
+    }
+}
