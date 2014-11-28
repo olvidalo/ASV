c3033200976439284ccc07768c304dd976a4bf73
Twan Goosen <t.goosen@gmail.com>
2014-11-28 09:42:57 +0000

    Added logic to look for streaming alternative in ../Streaming/{}.mp4 Also added the following note with a link view the non-streaming version:
    	'This is a streaming version of the selected media file that may be of reduced quality. Click [here] to see the original media.'
    
    Fixes #3838

--- metadata-browser-cmdi/src/main/java/nl/mpi/metadatabrowser/model/cmdi/wicket/components/MediaFilePanel.java
+++ metadata-browser-cmdi/src/main/java/nl/mpi/metadatabrowser/model/cmdi/wicket/components/MediaFilePanel.java
@@ -22,17 +22,21 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import nl.mpi.archiving.corpusstructure.core.CorpusNode;
+import nl.mpi.archiving.corpusstructure.core.OutputFormat;
 import nl.mpi.archiving.corpusstructure.core.service.NodeResolver;
 import nl.mpi.archiving.corpusstructure.provider.CorpusStructureProvider;
 import nl.mpi.metadatabrowser.model.NodeAction;
 import nl.mpi.metadatabrowser.model.TypedCorpusNode;
 import nl.mpi.metadatabrowser.services.URIFilter;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.ExternalLink;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -56,6 +60,16 @@ public final class MediaFilePanel extends Panel {
     private final static Logger logger = LoggerFactory.getLogger(NodeAction.class);
 
     /**
+     * location of the streaming file where %s is the filename without extension
+     * of the requested version
+     */
+    public final static String STREAMING_VERSION_LOCATION = "../Streaming/%s.mp4";
+    public final static String STREAMING_FILE_TYPE = "video/mp4";
+    public final static String STREAMING_FILE_EXTENSION = ".mp4";
+
+    private IModel<Boolean> allowStreamingModel = Model.of(Boolean.TRUE);
+
+    /**
      * Method that handle the html5 tag generation for video and build the
      * wicket front-end of the panel.
      *
@@ -73,13 +87,30 @@ public final class MediaFilePanel extends Panel {
 
             @Override
             public boolean isVisible() {
-                return html5media == null;
+                return html5media == null || !allowStreamingModel.getObject();
             }
 
         });
 
+        final WebMarkupContainer streaming = new WebMarkupContainer("streaming") {
+
+            @Override
+            public boolean isVisible() {
+                return html5media != null && allowStreamingModel.getObject();
+            }
+        };
+        add(streaming);
+
+        streaming.add(new Link("original") {
+
+            @Override
+            public void onClick() {
+                allowStreamingModel.setObject(Boolean.FALSE);
+            }
+        });
+        
         // add view for case where HTML5 resources is available
-        Html5Video video = (new Html5Video("displayVideo", createMediaModel(html5media)) {
+        final Html5Video video = (new Html5Video("displayVideo", createMediaModel(html5media)) {
             private static final long serialVersionUID = 1L;
 
             @Override
@@ -92,34 +123,35 @@ public final class MediaFilePanel extends Panel {
                 return true;
             }
 
-            @Override
-            public boolean isVisible() {
-                return html5media != null;
-            }
-
         });
+        streaming.add(video);
 
-        add(video);
         add(new ExternalLink("viewVideo", getNodeUrl(node)));
     }
 
     private List<MediaSource> findHtml5alternative(TypedCorpusNode node) {
         final URI nodeParent = csdb.getCanonicalParent(node.getNodeURI());
-        final String parentName = getFilename(node);
+        final File localFile = resolver.getLocalFile(node);
+        final String parentName = getFilename(node, localFile);
         final String parentBaseName = FilenameUtils.removeExtension(parentName);
         final List<CorpusNode> childrenNodes = csdb.getChildNodes(nodeParent);
 
         final List<MediaSource> mm = new ArrayList<>();
-        for (CorpusNode childNode : childrenNodes) { // parent has children
-            final String childNodeName = getFilename(childNode);
-            if (childNodeName.endsWith(".mp4")) { // look up for children with mp4 extension
-                // name comparison. Should be same node if only extension differs within the same session
-                final String childBaseName = FilenameUtils.removeExtension(childNodeName);
-                if (parentBaseName.equals(childBaseName)) {
-                    logger.debug("Found MP4 alternative for {}: {}", node, childNode);
-                    String childNodeURL = getNodeUrl(childNode);
-                    mm.add(new MediaSource(childNodeURL, "video/mp4"));
+        final URI streamingVersionURI = getStreamingVersionURI(parentBaseName, localFile, node);
+        if (streamingVersionURI != null) {
+            mm.add(new MediaSource(streamingVersionURI.toString()));
+        } else {
+            for (CorpusNode childNode : childrenNodes) { // parent has children
+                final String childNodeName = getFilename(childNode, resolver.getLocalFile(childNode));
+                if (childNodeName.endsWith(STREAMING_FILE_EXTENSION)) { // look up for children with mp4 extension
+                    // name comparison. Should be same node if only extension differs within the same session
+                    final String childBaseName = FilenameUtils.removeExtension(childNodeName);
+                    if (parentBaseName.equals(childBaseName)) {
+                        logger.debug("Found streaming alternative for {}: {}", node, childNode);
+                        String childNodeURL = getNodeUrl(childNode);
+                        mm.add(new MediaSource(childNodeURL, STREAMING_FILE_TYPE));
 //                  mm.add(new MediaSource(url in ogg format, "video/ogg.")); // ideally supported but not for now
+                    }
                 }
             }
         }
@@ -128,6 +160,26 @@ public final class MediaFilePanel extends Panel {
         } else {
             return mm;
         }
+    }
+
+    private URI getStreamingVersionURI(final String parentBaseName, final File localFile, TypedCorpusNode node) {
+        if (localFile != null) {
+            // try to find the streaming alternative on disk
+            final String streamingVersionRelativePath = String.format(STREAMING_VERSION_LOCATION, parentBaseName);
+            final URI streamingVersionLocalURI = localFile.toURI().resolve(streamingVersionRelativePath);
+            final File streamingVersionFile = new File(streamingVersionLocalURI);
+            // constructed a file location, now see if it exists
+            if (streamingVersionFile.exists()) {
+                try {
+                    // bingo, construct the public URL
+                    logger.debug("Found streaming alternative for {} at {}", localFile, streamingVersionFile);
+                    return resolver.getUrl(node).toURI().resolve(streamingVersionRelativePath);
+                } catch (URISyntaxException ex) {
+                    logger.error("Invalid URI while looking for streaming version of {}", localFile, ex);
+                }
+            }
+        }
+        return null;
     }
 
     private String getNodeUrl(CorpusNode node) {
@@ -143,8 +195,7 @@ public final class MediaFilePanel extends Panel {
         return nodeURL;
     }
 
-    private String getFilename(CorpusNode node) {
-        final File localFile = resolver.getLocalFile(node);
+    private String getFilename(CorpusNode node, File localFile) {
         if (localFile != null) {
             return localFile.getName();
         } else {
