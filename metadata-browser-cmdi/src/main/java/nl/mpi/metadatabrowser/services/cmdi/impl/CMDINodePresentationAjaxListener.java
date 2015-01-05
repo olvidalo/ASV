ae4a1fe4db74db57ffc025d807d44dadddde16e8
Twan Goosen <t.goosen@gmail.com>
2015-01-05 16:18:46 +0000

    Added a listener interface for the node presentation AJAX update. Implemented it so that the IMDI viewer html can be initialised properly and only when needed. Removed this init from the 'onload' event of the home page body.

---
+++ metadata-browser-cmdi/src/main/java/nl/mpi/metadatabrowser/services/cmdi/impl/CMDINodePresentationAjaxListener.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2015 Max Planck Institute for Psycholinguistics
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.mpi.metadatabrowser.services.cmdi.impl;
+
+import nl.mpi.metadatabrowser.wicket.NodePresentationAjaxListener;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+
+/**
+ *
+ * @author Twan Goosen <twan.goosen@mpi.nl>
+ */
+public class CMDINodePresentationAjaxListener implements NodePresentationAjaxListener {
+
+    @Override
+    public void onNodeSelectionChanged(AjaxRequestTarget target) {
+        target.appendJavaScript("init_viewer('imdi-viewer-open.png', 'imdi-viewer-closed.png')");
+    }
+
+}
