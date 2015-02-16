a0897a7044dcf0bed8bb3f5a7f353139938d57ab
Twan Goosen <t.goosen@gmail.com>
2015-02-16 16:29:45 +0000

    Merged fix for #4462 to 1.1 branch. Also updated CS2 reference to 1.4-SNAPSHOT refs #4462, #4507

--- metadata-browser-cmdi/src/main/java/nl/mpi/metadatabrowser/services/cmdi/mock/MockCmdiCorpusStructureDB.java
+++ metadata-browser-cmdi/src/main/java/nl/mpi/metadatabrowser/services/cmdi/mock/MockCmdiCorpusStructureDB.java
@@ -144,4 +144,9 @@ public class MockCmdiCorpusStructureDB implements CorpusStructureProvider, Seria
     public Collection<CorpusNode> getDescendantNodesByType(URI nodeUri, List<CorpusNodeType> types) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public boolean hasChildNodes(URI nodeUri) {
+        return getChildNodeCount(nodeUri) > 0;
+    }
 }
